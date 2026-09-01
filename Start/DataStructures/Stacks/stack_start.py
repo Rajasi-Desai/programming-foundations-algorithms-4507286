34275fc25735b9ba47967d8b33a010a303f7d51b
Rajasi Desai <rajasi.desai18@Gmail.com>
2026-09-01 08:12:53 +0000

    stacks exercise

--- Start/DataStructures/Stacks/stack_start.py
+++ Start/DataStructures/Stacks/stack_start.py
@@ -2,12 +2,27 @@
 # try out the Python stack functions
 
 # TODO: create a new empty stack
-
+stack = []
 
 # TODO: push items onto the stack
-
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
 
 # TODO: print the stack contents
+for i in stack:
+  print(i)
 
+#you can just do
+print(stack)
 
 # TODO: pop an item off the stack
+stack.pop()
+
+#solution
+#x = stack.pop()
+#print(x)
+
+# for i in stack:
+#   print(i)  
